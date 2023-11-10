3205fc5e4224346b2dd7ddd0e80dbf5f77f7f05c
Donnie Rawlings <tacobellboi@yahoo.com>
2023-11-09 20:30:42 -0600

    set up server.js file, and schema

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ const server = new ApolloServer({
 
 const startApolloServer = async () => {
   await server.start();
-};
+
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -38,5 +38,11 @@ app.get('*', (req, res) => {
 
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}$/graphql`);
+  });
 });
+}
+
+startApolloServer();
