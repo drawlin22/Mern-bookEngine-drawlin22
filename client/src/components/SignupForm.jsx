8c85be00649fbb97fd6a82d6e816dfa4a20fcd98
Donnie Rawlings <tacobellboi@yahoo.com>
2023-11-10 14:42:38 -0600

    changed App.jsx to be inside appolo server

--- client/src/components/SignupForm.jsx
+++ client/src/components/SignupForm.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
 
-// import { createUser } from '../utils/API';
-
-import { useMutation, useQuery } from '@apollo/client';
-import { ADD_USER,LOGIN_IN, SAVE_BOOK, REMOVE_BOOK } from '../utils/mutations';
+import { useMutation} from '@apollo/client';
+import { ADD_USER} from '../utils/mutations';
 
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
+  const [addUser, { data }] = useMutation(ADD_USER);
   // set initial form state
-  const [ADD_USER, setUserFormData] = useState({ username: '', email: '', password: '' });
+  const [userFormData, setUserFormData] = useState({ username: '', email: '', password: '' });
   // set state for form validation
   const [validated] = useState(false);
   // set state for alert
@@ -32,13 +31,13 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
+      const response = await addUser({variables: userFormData});
 
       if (!response.ok) {
         throw new Error('something went wrong!');
       }
 
-      const { token, user } = await response.json();
+      const { token, user } = await data.addUser;
       console.log(user);
       Auth.login(token);
     } catch (err) {
